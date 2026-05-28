90b75d67a88b7c794fe3703664ab17e27e9e8130
JoseMiguel <l>
2026-05-28 17:41:29 -0600

    chore(release): 1.1.0

--- src/config/queryClient.ts
+++ src/config/queryClient.ts
@@ -4,6 +4,7 @@ import { QueryClient } from '@tanstack/react-query'
 export const CACHE_24H = 1000 * 60 * 60 * 24
 export const CACHE_5MIN = 1000 * 60 * 5
 export const CACHE_15MIN = 1000 * 60 * 15
+// .-
 
 export const queryClient = new QueryClient({
   defaultOptions: {
